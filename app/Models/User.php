b8669c7072f15bc99b8633c4f4fd92bfcf6325b8
ys-grouse <malsawma7777@gmail.com>
2024-07-23 23:24:47 +0530

    fix[add]: can access panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
     use SoftDeletes;
@@ -48,6 +50,10 @@ class User extends Authenticatable
     }
 
     public function canAccessFilament(): bool
+    {
+        return true;
+    }
+    public function canAccessPanel(Panel $panel): bool
     {
         return true;
     }
