7c7f5d81330fef994d8de8fb62b23c411f013c15
ys-grouse <malsawma7777@gmail.com>
2024-07-24 14:52:20 +0530

    chore[add]: hotel relation for menu item

--- database/migrations/2024_07_23_145840_create_menu_items_table.php
+++ database/migrations/2024_07_23_145840_create_menu_items_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Hotel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,7 @@ return new class extends Migration
             $table->string('image_path')->nullable();
             $table->string('description')->nullable();
             $table->decimal('price')->nullable();
+            $table->foreignIdFor(Hotel::class)->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
